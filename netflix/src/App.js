3bda26b55ff90a4d98da27aa9b358fda7b3c3001
victorsamir-bandtec <victor.bsilva@bandtec.com.br>
2020-10-06 10:16:43 -0300

    finalizado, parte de pegar dados

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import tmdb from './tmdb';
 
 function App() {
+
+  useEffect(() => {
+
+    const loadAll = async () => {
+      const list = await tmdb.getHomeList();
+      console.log(list);
+    }
+
+    loadAll();
+
+  }, []);
+
   return (
     <h1>samir</h1>
   );
--- netflix/src/tmdb.js
+++ netflix/src/tmdb.js
@@ -1,20 +1,10 @@
-import { Suspense } from "react";
-
 const apiKey = "470ea88864fa07fdee90a06a14d03fb2";
 
 const apiBase = "https://api.themoviedb.org/3";
 
-//   - originais netflix
-//   - recomendados
-//   - Suspense
-//   - comedia
-//   - drama
-//   - aventura
-
-
 const pegandoDados = async (url) => {
 
-  const req = await fetch(`${apiBase} ${url}`)
+  const req = await fetch(`${apiBase}${url}`)
   const json = await req.json();
 
   return json;
@@ -40,33 +30,33 @@ export default {
       },
 
       {
-        genero: 'aventura',
-        titulo: 'Aventura',
-        itens: []
+        genero: 'ação',
+        titulo: 'Ação',
+        itens: await pegandoDados(`/discover/movie?with_generes=28&language=pt-BR&api_key=${apiKey}`)
       },
 
       {
         genero: 'terror',
         titulo: 'Terror',
-        itens: []
+        itens: await pegandoDados(`/discover/movie?with_generes=27&language=pt-BR&api_key=${apiKey}`)
       },
 
       {
         genero: 'comedia',
         titulo: 'Comedia',
-        itens: []
+        itens: await pegandoDados(`/discover/movie?with_generes=35&language=pt-BR&api_key=${apiKey}`)
       },
 
       {
         genero: 'drama',
         titulo: 'Drama',
-        itens: []
+        itens: await pegandoDados(`/discover/movie?with_generes=18&language=pt-BR&api_key=${apiKey}`)
       },
 
       {
-        genero: 'suspense',
-        titulo: 'Suspense',
-        itens: []
+        genero: 'Ficção',
+        titulo: 'Ficção Cientifica',
+        itens: await pegandoDados(`/discover/movie?with_generes=878&language=pt-BR&api_key=${apiKey}`)
       }
 
     ];
